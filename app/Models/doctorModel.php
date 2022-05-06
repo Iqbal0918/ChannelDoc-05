4c9759d4264d3481eb6c13a5305958e2db5bfdef
Iqbal0918 <77451963+Iqbal0918@users.noreply.github.com>
2022-05-06 18:16:18 +0530

    Add files via upload

--- app/Models/doctorModel.php
+++ app/Models/doctorModel.php
@@ -8,6 +8,9 @@ class doctorModel extends  \CodeIgniter\Model
     
    protected $allowedFields=['doctor_id','Fname','Lname','NIC', 'Email','Password','Age','Gender','Address','district', 'Govt_RegNo', 'areaofexpertise', 'qualification', 'placeofwork', 'phonenumber','approval','authFile'];
     protected $returnType = 'App\Entities\doc'; 
+
+
 }
+
 
 ?>
--- app/Models/employeeModel.php
+++ app/Models/employeeModel.php
@@ -8,6 +8,8 @@ class employeeModel extends  \CodeIgniter\Model
     
    protected $allowedFields=['user_id','fname','Lname','Email','Title','Gender','Address','NIC','district','phonenumber','pwd','age'];
     protected $returnType = 'App\Entities\empl'; 
+
+
 }
 
 ?>
