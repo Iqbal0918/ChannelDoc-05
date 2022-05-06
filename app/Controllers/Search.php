3e333ad0cb8d08eeda420832fa5c4bc2499b6594
Iqbal0918 <77451963+Iqbal0918@users.noreply.github.com>
2022-05-06 18:12:19 +0530

    Add files via upload

--- app/Controllers/Search.php
+++ app/Controllers/Search.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\doctorModel;
+use App\Models\Appointments;
 
 class Search extends BaseController
 {
@@ -9,8 +11,18 @@ class Search extends BaseController
     {
         if (session()->get('user_id') == null) {
             return redirect()->to('login/index')->with('fail', 'You must be logged in..');
-          }
+        }
         return view('Search/index.php');
     }
+
+    public function writedb()
+    {
+        session();
+
+        $fname = session()->get('fname');
+        $appos = new \App\Entities\appos($this->request->getPost());        
+        $model = new \App\Models\Appointments();
+        $model->insert($appos);
+        return redirect()->to('Search/index')->with('success', 'Successful');
+    }
 }
-?>
